b67df2c460c53cc775f0deb3950c1f1e6c630a3a
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-06-27 17:14:18 +0530

    Method scale_features created

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,12 +1,12 @@
 # Nodule for loading and cleaning the raw data
 # Handle missing values
 # Remove/Impute outliers
-# Normalize features(MnMax/Standard)
+# Normalize features(MinMax/Standard)
 # Split data into train/test(by time)
 
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing  import MinMaxScaler
+from sklearn.preprocessing  import StandardScaler
 import os
 
 class DataPreprocessor:
@@ -41,9 +41,51 @@ class DataPreprocessor:
     def handle_missing_values(self):
         # Handles missing values using forward fill('ffill) which is best for time series.
         print("\nHandling missing values using forward fill('ffill'):")
-        self.df.fillna(method='ffill', inplace=True)
+        self.df.ffill(inplace=True)
         print("Missing value handling complete.")
         self.detect_missing_values()
+
+    def handle_outliers(self):
+        """
+        Caps outliers in the specified column using the IQR method.
+        Creates a new column '<column>'_capped
+
+        """
+        q1 = self.df['Appliances'].quantile(0.25)
+        q3 = self.df['Appliances'].quantile(0.75)
+
+        iqr = q3 - q1
+
+        lower_bound = q1 - 1.5 * iqr
+        upper_bound = q3 + 1.5 * iqr
+
+        before_min = self.df['Appliances'].min()  # Min before capping outliers
+        before_max = self.df['Appliances'].max()  # Max before capping outliers
+
+        self.df['Appliances_capped']  = self.df['Appliances'].clip(lower=lower_bound, upper=upper_bound)
+
+        after_min = self.df['Appliances_capped'].min() # Min after capped the outliers
+        after_max = self.df['Appliances_capped'].max() # Max after capped the outliers
+
+        print(f"\nCapped 'Appliances_capped' outliers at [{lower_bound:.2f}, {upper_bound:.2f}].")
+        print(f"\nBefore capping: min={before_min}, max={before_max}")
+        print(f"\nAfter capping: min={after_min}, max={after_max}")
+
+    def scale_features(self, columns):
+        """
+        Scale specific columns using StandardScaler.
+        Creates new columns with '_scaled' suffix.
+
+        """
+        scaler = StandardScaler()
+        scaled_values = scaler.fit_transform(self.df[columns])
+
+        for i, col in enumerate(columns):
+            self.df[col  + '_scaled'] = scaled_values[:, i]
+        print(f"\nScaled columns: {columns} (zero mean unit variance)")
+
+
+
 
     
 
@@ -62,6 +104,9 @@ if __name__ == "__main__":
     processor = DataPreprocessor("../data/raw/energy_data_set.csv")
     #print(processor.df.columns)
     #print(processor.df.info())
+    processor.detect_missing_values()
     processor.handle_missing_values()
+    processor.handle_outliers()
+    # processor.scale_features(columns=['SNM'])
     print(processor.df.head(3))
  
