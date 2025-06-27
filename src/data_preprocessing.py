487e8ed8ac89dcb765584b85008f0ee7432b9184
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-06-27 15:28:03 +0530

    Methods created to detect & handle missing values

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -29,12 +29,39 @@ class DataPreprocessor:
         df = pd.read_csv(self.file_path)
         df['date'] = pd.to_datetime(df['date'])
         df.set_index('date', inplace=True)
+        print("Data loaded. Shape:", df.shape)
         return df
+    
+    def detect_missing_values(self):
+        # Prints  & return the number of missing values per column.
+        missing_values = self.df.isnull().sum()
+        print("\nMissing values per column:\n", missing_values)
+        return missing_values
+    
+    def handle_missing_values(self):
+        # Handles missing values using forward fill('ffill) which is best for time series.
+        print("\nHandling missing values using forward fill('ffill'):")
+        self.df.fillna(method='ffill', inplace=True)
+        print("Missing value handling complete.")
+        self.detect_missing_values()
+
+    
+
+
+
+
+
+
+
+
+
 
 # Example Usage/Test code(Remove these lines when importing in main code)
 
 if __name__ == "__main__":
     processor = DataPreprocessor("../data/raw/energy_data_set.csv")
-    print(processor.df.head())
-    print(processor.df.columns)
-    print(processor.df.info())
+    #print(processor.df.columns)
+    #print(processor.df.info())
+    processor.handle_missing_values()
+    print(processor.df.head(3))
+ 
